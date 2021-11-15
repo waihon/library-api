0a45266eeeb5323425a439600dbd0a407fea88b7
Waihon Yew <yewwaihon@gmail.com>
2021-11-15 13:25:42 +0800

    Add seed data for authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create!([
+  {
+    first: 'J.K.',
+    last: 'Rowling'
+  },
+  {
+    first: 'Stephen',
+    last: 'King'
+  },
+  {
+    first: 'Mark',
+    last: 'Twain'
+  },
+  {
+    first: 'Ernest',
+    last: 'Hemingway'
+  },
+  {
+    first: 'Jane',
+    last: 'Austen'
+  },
+  {
+    first: 'Charles',
+    last: 'Dickens'
+  },
+  {
+    first: 'Virginia',
+    last: 'Woolf'
+  },
+  {
+    first: 'William',
+    last: 'Shakespeare'
+  },
+  {
+    first: 'Edgar Allan',
+    last: 'Poe'
+  },
+  {
+    first: 'John',
+    last: 'Schatz'
+  }
+])
