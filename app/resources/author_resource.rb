5a6ad4c58e21e26f2273e1bf053e7b6dbaad8ca9
Waihon Yew <yewwaihon@gmail.com>
2021-11-15 13:27:28 +0800

    Query authors by exact match of last name

--- app/resources/author_resource.rb
+++ app/resources/author_resource.rb
@@ -1,3 +1,14 @@
 class AuthorResource < JSONAPI::Resource
   attributes :first, :last
+  filters :query
+
+  def self.apply_filter(records, filter, value, options)
+    case filter
+    when :query
+      # `value` is an array. We're interested in its first element.
+      records.where({ last: value.first })
+    else
+      super
+    end
+  end
 end
