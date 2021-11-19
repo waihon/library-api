0741a4cede23de8a230dade60ed9ed43809865fc
Waihon Yew <yewwaihon@gmail.com>
2021-11-20 00:19:08 +0800

    Update database schema by running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_130840) do
+ActiveRecord::Schema.define(version: 2021_11_19_152629) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 2021_11_10_130840) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "isbn"
+    t.date "publish_date"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
