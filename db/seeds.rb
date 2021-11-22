3ff3e9ec8ceae41902508caef99f99911eb412d4
Waihon Yew <yewwaihon@gmail.com>
2021-11-22 17:44:41 +0800

    Add seed data for books with association to authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Author.create!([
+rowling = Author.create!(first: 'J.K.', last: 'Rowling')
+king = Author.create!(first: 'Stephen', last: 'King')
+twain = Author.create!(first: 'Mark', last: 'Twain')
+hemingway = Author.create!(first: 'Ernest', last: 'Hemingway')
+austen = Author.create!(first: 'Jane', last: 'Austen')
+dickens = Author.create!(first: 'Charles', last: 'Dickens')
+woolf = Author.create!(first: 'Virginia', last: 'Woolf')
+shakespeare = Author.create!(first: 'William', last: 'Shakespeare')
+poe = Author.create!(first: 'Edgar Allan', last: 'Poe')
+schatz = Author.create!(first: 'John', last: 'Schatz')
+
+Book.create([
   {
-    first: 'J.K.',
-    last: 'Rowling'
+    title: "For Whom the Bell Tolls",
+    isbn: "978-0684803357",
+    publish_date: "1995-07-01",
+    author: hemingway
   },
   {
-    first: 'Stephen',
-    last: 'King'
+    title: "The Old Man and the Sea",
+    isbn: "0684801221",
+    publish_date: "1995-05-05",
+    author: hemingway
   },
   {
-    first: 'Mark',
-    last: 'Twain'
+    title: "The Dark Tower: The Gunslinger",
+    isbn: "978-0937986509",
+    publish_date: "1998-01-01",
+    author: king
   },
   {
-    first: 'Ernest',
-    last: 'Hemingway'
+    title: "The Green Mile",
+    isbn: "978-0671041786",
+    publish_date: "1999-11-01",
+    author: king
   },
   {
-    first: 'Jane',
-    last: 'Austen'
+    title: "Harry Potter and the Chamber of Secrets",
+    isbn: "0439064872",
+    publish_date: "2000-09-01",
+    author: rowling
   },
   {
-    first: 'Charles',
-    last: 'Dickens'
+    title: "The Adventures of Tom Sawyer",
+    isbn: "979-8749478112",
+    publish_date: "2021-05-05",
+    author: twain
   },
   {
-    first: 'Virginia',
-    last: 'Woolf'
-  },
-  {
-    first: 'William',
-    last: 'Shakespeare'
-  },
-  {
-    first: 'Edgar Allan',
-    last: 'Poe'
-  },
-  {
-    first: 'John',
-    last: 'Schatz'
+    title: "Pride and Prejudice",
+    isbn: "978-0141439518",
+    publish_date: "2002-12-31",
+    author: austen
   }
 ])
