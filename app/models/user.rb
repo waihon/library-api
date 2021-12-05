a8623e9256d53f747da80171cc9f5a6978c101b7
Waihon Yew <yewwaihon@gmail.com>
2021-12-05 12:49:30 +0800

    Implement user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+
+  def password=(unencrypted_password)
+    @password = unencrypted_password
+    self.password_digest = BCrypt::Password.create(unencrypted_password)
+  end
 end
