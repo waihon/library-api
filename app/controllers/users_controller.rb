1e5ce33cd052fa6c9c699237a6e279c125da5257
Waihon Yew <yewwaihon@gmail.com>
2021-12-06 00:35:11 +0800

    Use the application-wide authenticate for showing current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate, only: [:show_current]
+
   def show_current
-    begin
-      authenticate_or_request_with_http_token do |token, options|
-        # Specifying true to validate the expiration time
-        verified_token = JWT.decode token, JWT_SECRET, true, { algorithm: "HS512" }
+    # Setting nil as the context since we don't need to modify this any more with
+    # our request data.
+    user_resource = UserResource.new(@current_user, nil)
 
-        # JWT.decode returns an array, and the "sub" key stores the user id
-        user_id = verified_token[0]["sub"]
+    resource_serializer = JSONAPI::ResourceSerializer.new(UserResource)
+    # Replacing serialize_to_hash with object_hash as per:
+    # https://github.com/cerebris/jsonapi-resources/issues/1149#issuecomment-657913006
+    json = resource_serializer.object_hash(user_resource, nil)
 
-        @current_user = User.find(user_id)
-      end
-
-      resource_serializer = JSONAPI::ResourceSerializer.new(UserResource)
-      # Setting nil as the context since we don't need to modify this any more with
-      # our request data.
-      user_resource = UserResource.new(@current_user, nil)
-      # Replacing serialize_to_hash with object_hash as per:
-      # https://github.com/cerebris/jsonapi-resources/issues/1149#issuecomment-657913006
-      json = resource_serializer.object_hash(user_resource, nil)
-
-      render json: json
-    rescue => exception
-      render status: :unauthorized, json: {
-        errors: [
-          {
-            status: 401,
-            title: "Unauthorized"
-          }
-        ]
-      }
-    end
+    render json: json
   end
 end
