28bded38ac892ee7bf2bd23409fc4fc9fa0b57a5
Waihon Yew <yewwaihon@gmail.com>
2021-12-05 21:35:34 +0800

    Validate the password passed in matches with the hashed password stored in the database

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,21 +6,34 @@ class SessionController < ApplicationController
     password = params["password"]
 
     begin
-      user = User.find_by!(email: email)
-      data = { id: user.id, username: user.username, email: user.email }
-      payload = { data: data, sub: user.id, exp: Time.now.to_i + 2 * 3600 } # 2 hours
+      user = User.find_by!(email: email) # using ! will raise an exception when not found
 
-      token = JWT.encode payload, JWT_SECRET, "HS512" # an HMAC algorithm
+      # This may look counter-intuitive but it's actually how a hashed password is compared
+      # with a clear password.
+      if BCrypt::Password.new(user.password_digest) == password
+        data = { id: user.id, username: user.username, email: user.email }
+        payload = { data: data, sub: user.id, exp: Time.now.to_i + 2 * 3600 } # 2 hours
 
-      render json: { token: token }
+        token = JWT.encode payload, JWT_SECRET, "HS512" # an HMAC algorithm
+
+        render json: { token: token }
+      else
+        render_unauthorized
+      end
     rescue => exception
-      render status: :unauthorized, json: {
-        errors: [
-          status: 401,
-          title: "Unauthorized",
-          detail: "Error logging in user with that email and password"
-        ]
-      }
+      render_unauthorized
     end
+  end
+
+private
+
+  def render_unauthorized
+    render status: :unauthorized, json: {
+      errors: [
+        status: 401,
+        title: "Unauthorized",
+        detail: "Error logging in user with that email and password"
+      ]
+    }
   end
 end
