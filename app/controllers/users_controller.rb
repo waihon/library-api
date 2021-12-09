e094ccf43b1e3a9dd9759790ce4e6f4b5c6ef3f5
Waihon Yew <yewwaihon@gmail.com>
2021-12-10 00:27:43 +0800

    Update users controller to use the methods of jsonapi-resources 0.9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,19 @@ class UsersController < ApplicationController
     user_resource = UserResource.new(@current_user, nil)
 
     resource_serializer = JSONAPI::ResourceSerializer.new(UserResource)
+    # jsonapi-resources 0.9
+    json = resource_serializer.serialize_to_hash(user_resource)
+    # jsonapi-resources 0.10
     # Replacing serialize_to_hash with object_hash as per:
     # https://github.com/cerebris/jsonapi-resources/issues/1149#issuecomment-657913006
-    json = resource_serializer.object_hash(user_resource, nil)
+    # json = resource_serializer.object_hash(user_resource, nil)
 
+    # jsonapi-resources 0.9
+    render json: json
+    # jsonapi-resources 0.10
     # Unlike serialize_to_hash, object_hash doesn't add any data key.
     # So, we'll have to manually add such key in order for the JSON response
     # to be a valid JSON API document.
-    render json: { data: json }
+    # render json: { data: json }
   end
 end
