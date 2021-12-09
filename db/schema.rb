8a53aaf7427f9b5acbe52d02ffa041f1faad81d4
Waihon Yew <yewwaihon@gmail.com>
2021-12-09 21:42:24 +0800

    Run migration and update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_041303) do
+ActiveRecord::Schema.define(version: 2021_12_09_130636) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
     t.string "last"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_authors_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -26,7 +28,9 @@ ActiveRecord::Schema.define(version: 2021_12_05_041303) do
     t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -46,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_12_05_041303) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "authors", "users"
   add_foreign_key "books", "authors"
+  add_foreign_key "books", "users"
   add_foreign_key "reviews", "books"
 end
