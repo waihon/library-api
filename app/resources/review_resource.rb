d482aca00de3d493bdcae9713f9bfdc3badc9a55
Waihon Yew <yewwaihon@gmail.com>
2021-12-10 00:28:42 +0800

    Update review resource to eager load users and define a computed username

--- app/resources/review_resource.rb
+++ app/resources/review_resource.rb
@@ -1,4 +1,17 @@
 class ReviewResource < JSONAPI::Resource
-  attributes :user, :body, :created_at
+  attributes :body, :created_at, :username
   has_one :book
+
+  def username
+    @model.user.username
+  end
+
+  def self.records(options = {})
+    # Eager load users
+    super.includes(:user)
+  end
+
+  before_save do
+    @model.user_id = context[:current_user].id if @model.new_record?
+  end
 end
