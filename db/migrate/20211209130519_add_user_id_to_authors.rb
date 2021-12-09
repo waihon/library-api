19f5b8b92a954177684eb2ac5a9a539d5f1f8b95
Waihon Yew <yewwaihon@gmail.com>
2021-12-09 21:23:04 +0800

    Remove 'null: false' from the generated migrations as there are existing authors and books

--- db/migrate/20211209130519_add_user_id_to_authors.rb
+++ db/migrate/20211209130519_add_user_id_to_authors.rb
@@ -1,5 +1,5 @@
 class AddUserIdToAuthors < ActiveRecord::Migration[6.1]
   def change
-    add_reference :authors, :user, null: false, foreign_key: true
+    add_reference :authors, :user, foreign_key: true
   end
 end
--- db/migrate/20211209130636_add_user_id_to_books.rb
+++ db/migrate/20211209130636_add_user_id_to_books.rb
@@ -1,5 +1,5 @@
 class AddUserIdToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :books, :user, null: false, foreign_key: true
+    add_reference :books, :user, foreign_key: true
   end
 end
