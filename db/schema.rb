1adfe4c422ca63c820024a2157a03e53909f2879
Waihon Yew <yewwaihon@gmail.com>
2021-12-09 22:20:31 +0800

    Run migration and update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_130636) do
+ActiveRecord::Schema.define(version: 2021_12_09_141533) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
@@ -39,7 +39,9 @@ ActiveRecord::Schema.define(version: 2021_12_09_130636) do
     t.integer "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_12_09_130636) do
   add_foreign_key "books", "authors"
   add_foreign_key "books", "users"
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
