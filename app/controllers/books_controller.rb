5628c79a8279d8f5ee946287f4198851310b7a00
Waihon Yew <yewwaihon@gmail.com>
2021-12-21 16:25:40 +0800

    Resolve runtime error by removing computed attributes for create and update endpoints

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,13 @@
 class BooksController < ApplicationController
   before_action :authenticate, only: [:create, :update, :destroy]
+  before_action :remove_computed_attributes, only: [:create, :update]
+
+  def remove_computed_attributes
+    computed_attributes = [:username]
+    computed_attributes.each do |attribute|
+      if params.dig(:data, :attributes, attribute)
+        params[:data][:attributes] = params[:data][:attributes].except attribute
+      end
+    end
+  end
 end
